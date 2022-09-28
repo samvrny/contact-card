383bebc169e8f0858b65779acf0c864d49b9e42a
samvrny <samvrny@gmail.com>
2022-09-28 13:07:33 -0500

    added CRUD features to the indexeddb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,3 +16,76 @@ export const initdb = async () => {
         }
     })
 }
+
+export const getDb = async () => {
+    //create a connection to the indexeddb database and the version we want to use
+    const contactDb = await openDB('contact_db', 1);
+
+    //create a new transaction and specify the store and data privledges
+    const tx = contactDb.transaction('contacts', 'readonly');
+
+    //open up the desired object store
+    const store = tx.objectStore('contacts');
+
+    //user the .getAll() method the get all data in the database
+    const request = store.getAll();
+
+    //get confirmation of the request
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+}
+
+export const postDb = async (name, email, phone, profile) => {
+    //create a connection to the database and specify the version we want to use
+    const contactDb = await openDB('contact_db', 1);
+
+    //create a new transaction and specify the store and data privledges
+    const tx = contactDb.transaction('contacts', 'readwrite');
+
+    //open up the desired object store
+    const store = tx.objectStore('contacts');
+
+    //use the .add() method on the store and pass in the content
+    const request = store.add({ name: name, email: email, phone: phone, profile: profile });
+
+    //get confirmation of the request
+    const result = await request;
+    console.log('Data saved to the database', result);
+}
+
+export const deleteDb = async (id) => {
+    //create a connection to the indexeddb database and the version we want to use
+    const contactDb = await openDB('contact_db', 1);
+
+    //create a new transaction and specify the store and data privledges
+    const tx = contactDb.transaction('contacts', 'readwrite');
+
+    //open up the desired object store
+    const store = tx.objectStore('contacts');
+
+    //use the .delete() method to get all data in the database
+    const request = store.delete(id);
+
+    //get confirmation of the request
+    const result = await request;
+    console.log('result.value', result);
+    return result?.value;
+}
+
+export const editDb = async (id, name, email, phone, profile) => {
+    //create a connection to the indexeddb database and the versionw e want to use
+    const contactDb = await openDB('contact_db', 1);
+
+    //create a new transaction and specify the store and data priveledges
+    const tx = contactDb.transaction('contacts', 'readwrite');
+
+    //open up the desired object store
+    const store = tx.objectStore('contacts');
+
+    //use the .put() method on the store and pass in the content
+    const request = store.put({ id: id, name: name, email: email, phone: phone, profile: profile });
+
+    const result = await request;
+    console.log('Database updated', result);
+}
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,19 +1,93 @@
-import "./form";
-import "./submit";
+import "./form"; // may not need, or need to eliminate
 
 import '../css/index.css';
-import { Tooltip, Toast, Popover  } from "bootstrap";
+import { Tooltip, Toast, Popover } from "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
-import { initdb } from './database';
+import { initdb, getDb, postDb, deleteDb, editDb } from './database';
+import { fetchCards } from './cards';
+import { toggleForm, clearForm } from './form';
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     initdb();
+    fetchCards();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
 });
+
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+    toggleForm()
+})
+
+form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+    if (submitBtnToUpdate == false) {
+        postDb(name, email, phone, profile);
+    } else {
+        //obtains values passed into the form element
+        let name = document.getElementById('name').value;
+        let phone = document.getElementById('phone').value;
+        let email = document.getElementById('email').value;
+        let profile = document.querySelector('input[type="radio"]:checked').value;
+
+        //calls the editDb function passing in any values from the form element as well as the ID of the contact that we are updating
+        editDb(profileId, name, email, phone, profile);
+        
+        fetchCards();
+        // Toggles the submit button back to POST functionality
+        submitBtnToUpdate = false;
+    }
+
+    // Clear form
+    clearForm();
+    // Toggle form
+    toggleForm();
+    // Reload the DOM
+    fetchCards();
+});
+
+window.deleteCard = (e) => {
+    //grabs the id from the button element attached to the contact card
+    let id = parseInt(e.id);
+    //delete the card 
+    deleteDb(id);
+    //reload the DOM
+    fetchCards();
+};
+
+window.editCard = (e) => {
+    //grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element
+    profileId = parseInt(e.dataset.id);
+
+    //grabs information to pre-populate edit form
+    let editName = e.dataset.name;
+    let editEmail = e.dataset.email;
+    let editPhone = e.dataset.phone;
+
+    document.getElementById('name').value = editName;
+    document.getElementById('email').value = editEmail;
+    document.getElementById('phone').value = editPhone;
+
+    form.style.display = 'block';
+
+    //toggles the submit button so that is now updates an existing contact instead of posting a new one
+    submitBtnToUpdate = true;
+};
